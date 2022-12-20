107e1c7ff130a4538d8f0b09bc1950697954fcb2
Unravelled0 <namazdervosov@mail.ru>
2022-12-20 20:28:44 +0400

    added sign up page and functionality

--- server.js
+++ server.js
@@ -3,18 +3,70 @@ const admin = require('firebase-admin');
 const bcrypt = require('bcrypt');
 const path = require('path');
 
+
+
+let serviceAccount = require("./finalproject-7be14-firebase-adminsdk-20zph-34aa55f3c1.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+let db = admin.firestore();
+
 let staticPath = path.join(__dirname,"public");
 
 const app = express();
 app.use(express.static(staticPath));
+app.use(express.json());
 
 app.get("/",(req,res)=>{
     res.sendFile(path.join(staticPath,"index.html"));
 });
 
-app.get("/signup", (req, res) => {
+app.get("/signup",(req, res)=>{
     res.sendFile(path.join(staticPath, "signup.html"));
 });
+
+app.post('/signup', (req, res) => {
+    let { name, email, password, number, tac, notification } = req.body;
+
+    // form validations
+    if(name.length < 3){
+        return res.json({'alert': 'name must be 3 letters long'});
+    } else if(!email.length){
+        return res.json({'alert': 'enter your email'});
+    } else if(password.length < 8){
+        return res.json({'alert': 'password should be 8 letters long'});
+    } else if(!number.length){
+        return res.json({'alert': 'enter your phone number'});
+    } else if(!Number(number) || number.length < 10){
+        return res.json({'alert': 'invalid number, please enter valid one'});
+    } else if(!tac){
+        return res.json({'alert': 'you must agree to our terms and conditions'});
+    }
+    db.collection('users').doc(email).get()
+.then(user => {
+    if(user.exists){
+        return res.json({'alert': 'email already exists'});
+    } else{
+        // encrypt the password before storing it.
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(password, salt, (err, hash) => {
+                req.body.password = hash;
+                db.collection('users').doc(email).set(req.body)
+                .then(data => {
+                    res.json({
+                        name: req.body.name,
+                        email: req.body.email,
+                        seller: req.body.seller,
+                    })
+                })
+            })
+        })
+    }
+})
+    // res.json('data recieved')       
+})
 
 app.listen(3000,()=>{
     console.log('listening on port 3000');
